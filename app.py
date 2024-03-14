d4cb66dd92dc6d48546be75a17ad7c916567541d
Lakshya <jklagrawal123@gmail.com>
2024-03-14 20:24:29 +0530

    Updates

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///problem_set.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(100), nullable=False)
+problem_set_db = SQLAlchemy(app)
+user_db = SQLAlchemy(app)
+
+class Problem(problem_set_db.Model):
+    id = problem_set_db.Column(problem_set_db.Integer, primary_key=True)
+    title = problem_set_db.Column(problem_set_db.String(100), nullable=False)
+    description = problem_set_db.Column(problem_set_db.String(2000), nullable=False)
+    sample_input = problem_set_db.Column(problem_set_db.String(100), nullable=False)
+    sample_output = problem_set_db.Column(problem_set_db.String(100), nullable=False)
+    exp_time = problem_set_db.Column(problem_set_db.Integer, nullable=False)
+    exp_space = problem_set_db.Column(problem_set_db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Id %r>' % self.id
+
+class user(user_db.Model):
+    id = user_db.Column(user_db.Integer, primary_key=True)
+    name = user_db.Column(user_db.String(100), nullable=False)
+    type = user_db.Column(user_db.String(100), nullable=False)
     
     def __repr__(self):
         return '<Id %r>' % self.id
@@ -16,9 +32,40 @@ class User(db.Model):
 def home():
     return render_template('home.html')
 
-@app.route('/judge',methods = ['GET'])
-def post_problems():
-    return render_template('judge.html')
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.form == 'POST':
+        newUser = user()
+        newUser.name = request.form['name']
+        newUser.type = request.form['type']
+
+        try:
+            user_db.session.add(newUser)
+            user_db.session.commit(newUser)
+            return render_template('/')
+        except:
+            return "Unable to enter user to the Database"
+    else:
+        return render_template('login.html')
+
+@app.route('/judge/<int:id>',methods = ['POST'])
+def post_problems(id):
+    curr_user = user.query.get_or_404(id)
+    
+    newProblem = Problem()
+    newProblem.title = request.form['title']
+    newProblem.description = request.form['description']
+    newProblem.sample_input = request.form['sample_input']
+    newProblem.sample_output = request.form['sample_output']
+    newProblem.exp_time = request.form['exp_time']
+    newProblem.exp_space = request.form['exp_space']
+
+    try:
+        problem_set_db.session.add(newProblem)
+        problem_set_db.session.commit(newProblem)
+        return render_template('home.html')
+    except:
+        return "There is no such user found. Please Sign Up."
 
 @app.route('/contestant')
 def solve_problems():
