c458f1c7d0baf7d89064b3beeaeb6e9f6736b605
Lakshya <jklagrawal123@gmail.com>
2024-03-14 23:34:14 +0530

    Updates

--- app.py
+++ app.py
@@ -2,70 +2,80 @@ from flask import Flask,render_template,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///problem_set.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_BINDS'] = {'problems':'sqlite:///problem_set.db'}   
 
-problem_set_db = SQLAlchemy(app)
-user_db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-class Problem(problem_set_db.Model):
-    id = problem_set_db.Column(problem_set_db.Integer, primary_key=True)
-    title = problem_set_db.Column(problem_set_db.String(100), nullable=False)
-    description = problem_set_db.Column(problem_set_db.String(2000), nullable=False)
-    sample_input = problem_set_db.Column(problem_set_db.String(100), nullable=False)
-    sample_output = problem_set_db.Column(problem_set_db.String(100), nullable=False)
-    exp_time = problem_set_db.Column(problem_set_db.Integer, nullable=False)
-    exp_space = problem_set_db.Column(problem_set_db.Integer, nullable=False)
-
-    def __repr__(self):
-        return '<Id %r>' % self.id
-
-class user(user_db.Model):
-    id = user_db.Column(user_db.Integer, primary_key=True)
-    name = user_db.Column(user_db.String(100), nullable=False)
-    type = user_db.Column(user_db.String(100), nullable=False)
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
     
     def __repr__(self):
-        return '<Id %r>' % self.id
+        return f"{self.name} - {self.type}"
+    
+class Problem(db.Model):
+    __bind_key = 'problems'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    sample_input = db.Column(db.String(100), nullable=False)
+    sample_output = db.Column(db.String(100), nullable=False)
+    exp_time = db.Column(db.Integer, nullable=False)
+    exp_space = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"{self.title} - {self.description}"
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-    if request.form == 'POST':
+@app.route('/signin',methods=['GET','POST'])
+def signin():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('signin.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
         newUser = user()
         newUser.name = request.form['name']
-        newUser.type = request.form['type']
-
+        newUser.type = request.form['selection']
+        print(newUser.type)
         try:
-            user_db.session.add(newUser)
-            user_db.session.commit(newUser)
-            return render_template('/')
+            db.session.add(newUser)
+            db.session.commit()
+            return render_template('home.html')
         except:
             return "Unable to enter user to the Database"
     else:
-        return render_template('login.html')
+        return render_template('signup.html')
 
-@app.route('/judge/<int:id>',methods = ['POST'])
-def post_problems(id):
-    curr_user = user.query.get_or_404(id)
-    
-    newProblem = Problem()
-    newProblem.title = request.form['title']
-    newProblem.description = request.form['description']
-    newProblem.sample_input = request.form['sample_input']
-    newProblem.sample_output = request.form['sample_output']
-    newProblem.exp_time = request.form['exp_time']
-    newProblem.exp_space = request.form['exp_space']
+@app.route('/judge',methods = ['GET','POST'])
+def post_problems():
+    # curr_user = user.query.get_or_404(id)
 
-    try:
-        problem_set_db.session.add(newProblem)
-        problem_set_db.session.commit(newProblem)
-        return render_template('home.html')
-    except:
-        return "There is no such user found. Please Sign Up."
+    if request.method == 'POST':
+        newProblem = Problem()
+        newProblem.title = request.form['title']
+        newProblem.description = request.form['description']
+        newProblem.sample_input = request.form['sample_input']
+        newProblem.sample_output = request.form['sample_output']
+        newProblem.exp_time = request.form['exp_time']
+        newProblem.exp_space = request.form['exp_space']
+
+        try:
+            db.session.add(newProblem)
+            db.session.commit()
+            return render_template('home.html')
+        except:
+            return "Unable to enter the problem into the Database"
+    else:
+        return render_template('judge.html')
 
 @app.route('/contestant')
 def solve_problems():
