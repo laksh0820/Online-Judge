85860415c5964b0750c44bf46b749f85e030712d
Lakshya <jklagrawal123@gmail.com>
2024-03-15 20:25:05 +0530

    Updates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 
 started_app = 0
+fun = 0
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
