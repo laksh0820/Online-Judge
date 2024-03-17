ac6def078ad720460cf6bafab65aa11af84e8cc9
Lakshya <jklagrawal123@gmail.com>
2024-03-17 12:39:47 +0530

    Updates

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
-from flask import Flask,render_template,redirect,request
+from flask import Flask,render_template,redirect,request,flash,url_for
 from flask_sqlalchemy import SQLAlchemy
-
-started_app = 0
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,TextAreaField,RadioField
+from wtforms.validators import DataRequired,EqualTo,Email,Regexp
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin,login_user,login_required,LoginManager,current_user,logout_user
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///User.db'
 app.config['SQLALCHEMY_BINDS'] = {'problems':'sqlite:///problem_set.db'}   
-
+app.config['SECRET_KEY'] = 'projectzetaxksdkar37ro8hf83fh3892hmfijw38fh'
 db = SQLAlchemy(app)
 
-class user(db.Model):
-    name = db.Column(db.String(100), nullable=False,primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'signin'
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class SignUpForm(FlaskForm):
+    name = StringField("Name",validators=[DataRequired()])
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    password = PasswordField("Password",validators=[DataRequired(),EqualTo('confirm_password')])
+    confirm_password = PasswordField("Confirm-Password",validators=[DataRequired()])
+    type = RadioField("Type",validators=[DataRequired()],choices=[('Contestants','Contestants'),('Judge','Judge')])
+    submit = SubmitField("Submit")
+
+class SignInForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False,unique=True)
+    email = db.Column(db.String(100), nullable=False,unique=True)
     password = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(100), nullable=False)
     
@@ -37,39 +62,60 @@ def home():
 
 @app.route('/signin',methods=['GET','POST'])
 def signin():
-    if request.method == 'POST':
-        curr_user = user.query.get_or_404(request.form['name'])
-        if curr_user == None:
-            return "No Such User Found"
-        
-        if request.form['password'] != curr_user.password:
-            return "Wrong Password"
-        
-        return render_template('home.html',user=curr_user)
-    else:
-       return render_template('signin.html')
+    form = SignInForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password,form.password.data):
+                login_user(user)
+                flash("Login Successfully")
+                return redirect(url_for('signin'))
+            else:
+                flash("Wrong Password!! Try Again...")
+        else:
+            flash("User not Found!! Try Again...")
+    return render_template('signin.html',form=form)
+    
+# Add User
 @app.route('/signup',methods=['GET','POST'])
 def signup():
-    if request.method == 'POST':
-        newUser = user()
-        newUser.name = request.form['name']
-        newUser.email = request.form['email']
-        newUser.password = request.form['password']
-        newUser.type = request.form['selection']
-        try:
-            db.session.add(newUser)
-            db.session.commit()
-            return render_template('home.html',user=None)
-        except:
-            return "Unable to enter user to the Database"
-    else:
-        return render_template('signup.html') 
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is None:
+            newUser = User()
+            newUser.name = form.name.data
+            newUser.email = form.email.data
+            newUser.password = generate_password_hash(form.password.data,"sha256")
+            newUser.type = form.type.data
+            try:
+                db.session.add(newUser)
+                db.session.commit()
+            except:
+                return "Unable to enter User to the Database"
+        form.name.data = ''
+        form.email.data = ''
+        form.password.data = ''
+        form.confirm_password.data = ''
+        form.type.data = ''
+
+        flash("User Added Successfully!!")
+        return redirect(url_for('signup'))
+
+    return render_template('signup.html',form = form)
+
+@app.route('/logout',methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out Successfully")
+    return redirect(url_for('signin'))
 
 @app.route('/judge',methods = ['GET','POST'])
+@login_required
 def post_problems():
-    # curr_user = user.query.get_or_404(request.form['name'])
-
     if request.method == 'POST':
         newProblem = Problem()
         newProblem.title = request.form['title']
