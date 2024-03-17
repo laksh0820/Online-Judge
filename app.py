1a9f9f88611023db51639e95e739b81029ef3760
Lakshya <jklagrawal123@gmail.com>
2024-03-17 17:10:46 +0530

    Updates

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,redirect,request,flash,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,TextAreaField,RadioField
-from wtforms.validators import DataRequired,EqualTo,Email,Regexp
+from wtforms import IntegerField,StringField, PasswordField, SubmitField,TextAreaField,RadioField
+from wtforms.validators import DataRequired,EqualTo,Email
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin,login_user,login_required,LoginManager,current_user,logout_user
 
@@ -22,16 +22,25 @@ def load_user(id):
 
 class SignUpForm(FlaskForm):
     name = StringField("Name",validators=[DataRequired()])
-    email = StringField("Email",validators=[DataRequired(),Email()])
-    password = PasswordField("Password",validators=[DataRequired(),EqualTo('confirm_password')])
+    email = StringField("Email",validators=[DataRequired(),Email(message="Invalid email address (should be of the form something@example.com)")])
+    password = PasswordField("Password",validators=[DataRequired(),EqualTo('confirm_password',message="Password does not match to Confirm Password. Please retype")])
     confirm_password = PasswordField("Confirm-Password",validators=[DataRequired()])
     type = RadioField("Type",validators=[DataRequired()],choices=[('Contestants','Contestants'),('Judge','Judge')])
     submit = SubmitField("Submit")
 
 class SignInForm(FlaskForm):
-    email = StringField("Email",validators=[DataRequired(),Email()])
+    email = StringField("Email",validators=[DataRequired(),Email(message="Invalid email address (should be of the form something@example.com)")])
     password = PasswordField("Password",validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class ProblemForm(FlaskForm):
+    title = StringField("Title",validators=[DataRequired()])
+    description = TextAreaField("Description",validators=[DataRequired()])
+    sample_input = TextAreaField("Sample Input",validators=[DataRequired()])
+    sample_output = TextAreaField("Sample Output",validators=[DataRequired()])
+    exe_time = IntegerField("Expected Execution Time",validators=[DataRequired()])
+    exe_space = IntegerField("Expected Execution Space",validators=[DataRequired()])
+    submit = SubmitField("Submit")  
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -50,8 +59,8 @@ class Problem(db.Model):
     description = db.Column(db.String(2000), nullable=False)
     sample_input = db.Column(db.String(100), nullable=False)
     sample_output = db.Column(db.String(100), nullable=False)
-    exp_time = db.Column(db.Integer, nullable=False)
-    exp_space = db.Column(db.Integer, nullable=False)
+    exe_time = db.Column(db.Integer, nullable=False)
+    exe_space = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"{self.title} - {self.description}"
@@ -60,6 +69,7 @@ class Problem(db.Model):
 def home(): 
     return render_template('home.html',user=None)
 
+# Sign in to an existing user
 @app.route('/signin',methods=['GET','POST'])
 def signin():
     form = SignInForm()
@@ -69,22 +79,24 @@ def signin():
         if user:
             if check_password_hash(user.password,form.password.data):
                 login_user(user)
-                flash("Login Successfully")
+                flash("Logged in Successfully")
                 return redirect(url_for('signin'))
             else:
-                flash("Wrong Password!! Try Again...")
+                flash("Wrong Password!! Try Again",'error')
         else:
-            flash("User not Found!! Try Again...")
+            flash("User not Found!! Try Again",'error')
     return render_template('signin.html',form=form)
     
-# Add User
+# Create a new User
 @app.route('/signup',methods=['GET','POST'])
 def signup():
     form = SignUpForm()
+    
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user_email = User.query.filter_by(email=form.email.data).first()
+        user_name = User.query.filter_by(name=form.name.data).first()
 
-        if user is None:
+        if (user_email is None) and (user_name is None):
             newUser = User()
             newUser.name = form.name.data
             newUser.email = form.email.data
@@ -93,19 +105,23 @@ def signup():
             try:
                 db.session.add(newUser)
                 db.session.commit()
+                flash("User Added Successfully!!")
             except:
                 return "Unable to enter User to the Database"
-        form.name.data = ''
-        form.email.data = ''
-        form.password.data = ''
-        form.confirm_password.data = ''
-        form.type.data = ''
+            form.name.data = ''
+            form.email.data = ''
+            form.password.data = ''
+            form.confirm_password.data = ''
+            form.type.data = ''
+        elif user_name is None:
+            flash('This email already exits. Please sign in','error')
+        else:
+            flash('This name already exits. Please enter a different name','error')
 
-        flash("User Added Successfully!!")
         return redirect(url_for('signup'))
-
     return render_template('signup.html',form = form)
 
+# Logout of a user account
 @app.route('/logout',methods=['GET','POST'])
 @login_required
 def logout():
@@ -113,34 +129,45 @@ def logout():
     flash("Logged out Successfully")
     return redirect(url_for('signin'))
 
+# Post a new Problem
 @app.route('/judge',methods = ['GET','POST'])
 @login_required
 def post_problems():
-    if request.method == 'POST':
+    form = ProblemForm()
+
+    if form.validate_on_submit():
         newProblem = Problem()
-        newProblem.title = request.form['title']
-        newProblem.description = request.form['description']
-        newProblem.sample_input = request.form['sample_input']
-        newProblem.sample_output = request.form['sample_output']
-        newProblem.exp_time = request.form['exp_time']
-        newProblem.exp_space = request.form['exp_space']
+        newProblem.title = form.title.data
+        newProblem.description = form.description.data
+        newProblem.sample_input = form.sample_input.data
+        newProblem.sample_output = form.sample_output.data
+        newProblem.exe_time = form.exe_time.data
+        newProblem.exe_space = form.exe_space.data
 
         try:
             db.session.add(newProblem)
             db.session.commit()
-            return render_template('home.html',user=None)
+            flash("Problem added Successfully!!")
         except:
-            return "Unable to enter the problem into the Database"
-    else:
-        return render_template('judge.html')
+            flash("There is some problem in adding the problem",'error')
+        
+        form.title.data = ''
+        form.description.data = ''
+        form.sample_input.data = ''
+        form.sample_output.data = ''
+        form.exe_time.data = 0
+        form.exe_space.data = 0
 
-@app.route('/contestant', methods= ['GET' , 'POST'])    # Cause You suck
-def solve_problems():
-    if request.method == 'POST':
-        pass
-    else:
-        problems_list = Problem.query.all()
-        return render_template('contestant.html' , ProblemSet = problems_list)
+    return render_template('judge.html',form=form)
+
+# @app.route('/contestant', methods= ['GET' , 'POST'])
+# @login_required                                         # Cause You suck
+# def solve_problems():
+#     if request.method == 'POST':
+#         pass
+#     else:
+#         problems_list = Problem.query.all()
+#         return render_template('contestant.html' , ProblemSet = problems_list)
     
 # @app.route('/onlineIDE')
 # def online_coding():
