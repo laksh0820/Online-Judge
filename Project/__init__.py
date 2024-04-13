a928fdc0e0d0cf45daec5a0d4f7ea0bd6337f377
@kmohanb2003 <kmohanb2003@kgpian.iitkgp.ac.in>
2024-04-13 08:32:37 +0530

    Updates

--- Project/__init__.py
+++ Project/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
-import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
@@ -9,10 +8,5 @@ app.config['SQLALCHEMY_BINDS'] = {'problems':'sqlite:///problem_set.db','submiss
 app.config['SECRET_KEY'] = 'projectzetaxksdkar37ro8hf83fh3892hmfijw38fh'
 app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=20)
 db = SQLAlchemy(app)
-
-if (os.path.exists("../instance") is False):
-    from Project import app,db
-    app.app_context().push()
-    db.create_all()
 
 from Project import routes
