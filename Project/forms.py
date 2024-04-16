741ee5d306a15a554aa4cd0c2cf76cda1083a859
Lakshya <jklagrawal123@gmail.com>
2024-04-16 12:32:11 +0530

    Updates

--- Project/forms.py
+++ Project/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField,StringField, PasswordField, SubmitField,TextAreaField,RadioField,BooleanField
 from wtforms.validators import DataRequired,EqualTo,Email,Length,Regexp
+from wtforms.widgets import PasswordInput
 
 MIN_PASSWORD_LEN = 8
 MAX_PASSWORD_LEN = 12
@@ -20,7 +21,7 @@ class SignInForm(FlaskForm):
     submit = SubmitField("Submit")
 
 class ResetPasswordForm(FlaskForm):
-    new_password = PasswordField("Password",validators=[DataRequired(),Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN),EqualTo('confirm_password',message="Password does not match to Confirm Password. Please retype"),Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]",message="Password should contain atleast one lowercase character, one uppercase character,one number and one special character." )])
+    new_password = PasswordField("New Password",validators=[DataRequired(),Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN),EqualTo('confirm_password',message="Password does not match to Confirm Password. Please retype"),Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]",message="Password should contain atleast one lowercase character, one uppercase character,one number and one special character." )])
     confirm_password = PasswordField("Confirm-Password",validators=[DataRequired(),Length(min=MIN_PASSWORD_LEN,max=MAX_PASSWORD_LEN)])
     Reset = SubmitField("Reset")
 
